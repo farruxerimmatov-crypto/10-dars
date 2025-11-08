49177560450d0f0338bfe6d9a9cd832aad8151d7
farrux <[200~https://github.com/farruxerimmatov-crypto~>
2025-11-08 10:50:38 +0500

    init

---
+++ main.js/index.js
@@ -0,0 +1,19 @@
+document.getElementById("loadBtn").addEventListener("click", getUsers);
+
+function getUsers() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(data => {
+      const userList = document.getElementById("userList");
+      userList.innerHTML = ""; // avvalgisini tozalaydi
+
+      data.forEach(user => {
+        const li = document.createElement("li");
+        li.textContent = `${user.name} — ${user.email}`;
+        userList.appendChild(li);
+      });
+    })
+    .catch(error => {
+      console.log("Xatolik:", error);
+    });
+}
